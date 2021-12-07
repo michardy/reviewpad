50ae7b714309099ed478227bd57bad916e9ba822
Michael Hardy <98.michael.hardy@gmail.com>
2021-12-06 19:02:45 -0800

    Enable running under iframe and properly inject script into correct context

--- rp.js
+++ rp.js
@@ -1,1 +1,10 @@
-delete window.alert;
+// based on:
+// https://stackoverflow.com/questions/2963677/can-i-prevent-an-alert-with-a-google-chrome-extension
+
+let inject = "delete window.alert;";
+
+let disElem = document.createElement("script");
+disElem.textContent = inject;
+
+document.documentElement.appendChild(disElem);
+disElem.parentNode.removeChild(disElem);
